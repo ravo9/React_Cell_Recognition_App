a81d275d215fd0622ca1e833629c6f2949cc0d28
Rafal Ozog <rafalozog@gmail.com>
2019-12-14 17:35:56 +0000

    Initial screen prepared.

--- react-cell-recognition-app/src/App.js
+++ react-cell-recognition-app/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button } from "mdbreact";
+import logo from './logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header ">
+          <img src={logo} alt="logo" className="App-logo" />
+          <h1 className="App-title">Welcome to Your MDB React App</h1>
+        </header>
+        <p className="mb-2">The application is configured and ready to import our components.</p>
+        <Button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></Button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-cell-recognition-app/src/index.js
+++ react-cell-recognition-app/src/index.js
@@ -1,12 +1,157 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import * as serviceWorker from './serviceWorker';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      events: []
+    };
+  }
+  addEvent = () => {
+    var newArray = [...this.state.events];
+    newArray.push({
+      id: newArray.length ? newArray[newArray.length - 1].id + 1 : 1,
+      time: this.state.time,
+      title: this.state.title,
+      location: this.state.location,
+      description: this.state.description
+    });
+    this.setState({ events: newArray });
+    this.setState({
+      time: "",
+      title: "",
+      location: "",
+      description: ""
+    });
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  handleInputChange = inputName => value => {
+    const nextValue = value;
+
+    this.setState({
+      [inputName]: nextValue
+    });
+  };
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="12" className="mb-r">
+              <h2 className="text-uppercase text-center my-5">Cells Recognition App</h2>
+              <h4 className="my-3 text-center">Please upload your cells photo, to let the Articifial Intelligence to find, recognize,
+              and count them.</h4>
+              <div id="schedule-items">
+                {this.state.events.map(event => (
+                  <Event
+                    key={event.id}
+                    id={event.id}
+                    time={event.time}
+                    title={event.title}
+                    location={event.location}
+                    description={event.description}
+                  />
+                ))}
+              </div>
+              <MDBRow className="mb-4 my-5">
+                <MDBCol xl="6" md="6" className="mx-auto text-center">
+                  <MDBBtn color="info" rounded onClick={this.toggleModal}>
+                    Upload cells photo
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+
+        <MDBModal isOpen={this.state.modal} toggle={this.toggleModal}>
+          <MDBModalHeader
+            className="text-center"
+            titleClass="w-100 font-weight-bold"
+            toggle={this.toggleModal}
+          >
+            Upload cells photo
+          </MDBModalHeader>
+          <MDBModalBody>
+            <form className="mx-3 grey-text">
+              <MDBInput
+                name="photo"
+                label="Photo"
+                icon="fa-photo"
+                group
+                type="file"
+                getValue={this.handleInputChange("photo")}
+              />
+              <MDBInput
+                name="description"
+                label="Description (optional)"
+                icon="sticky-note"
+                group
+                type="textarea"
+                getValue={this.handleInputChange("description")}
+              />
+            </form>
+          </MDBModalBody>
+          <MDBModalFooter className="justify-content-center">
+            <MDBBtn
+              color="info"
+              onClick={() => {
+                this.toggleModal();
+                this.addEvent();
+              }}
+            >
+              Add
+            </MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+      </React.Fragment>
+    );
+  }
+}
+
+class Event extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="media mt-1">
+          <h3 className="h3-responsive font-weight-bold mr-3">
+            {this.props.time}
+          </h3>
+          <div className="media-body mb-3 mb-lg-3">
+            <h6 className="mt-0 font-weight-bold">{this.props.title} </h6>{" "}
+            <hr className="hr-bold my-2" />
+            {this.props.location && (
+              <React.Fragment>
+                <p className="font-smaller mb-0">
+                  <MDBIcon icon="location-arrow" /> {this.props.location}
+                </p>
+              </React.Fragment>
+            )}
+          </div>
+        </div>
+        {this.props.description && (
+          <p className="p-2 mb-4  blue-grey lighten-5 blue-grey lighten-5">
+            {this.props.description}
+          </p>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
